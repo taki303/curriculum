a8d85a603ebcb90b1fa5a32c177dee0dfc5d330e
taki303 <79926956+taki303@users.noreply.github.com>
2021-03-07 01:03:07 +0100

    提出用課題Check1
    提出用課題Check1

---
+++ JavaScript/Check1/main.js
@@ -0,0 +1,24 @@
+let numbers = [2, 5, 12, 13, 15, 18, 22];
+//ここに答えを実装してください。↓↓↓
+function isEven(num) {
+    if(num % 2 === 0) {
+    console.log(num + 'は偶数です');
+    }
+}
+for(i = 0; i < numbers.length; i ++){
+    isEven(i)
+}
+
+
+class car {
+    constructor(guss, number){
+        this.guss = guss;
+        this.number = number;
+    }
+    getNumGas(){
+        console.log(`ガソリンは${this.guss}です。ナンバーは${this.number}です。`);
+    }
+}
+
+let rentCar = new car(0.2, 1234);
+rentCar.getNumGas();
